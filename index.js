6421b943aaf89439b290b71d9d43226f3bed3908
Michael Davis <jrmdd1496@gmail.com>
2022-03-31 17:53:57 -0500

    commit tree 61eba9ebacdf22c26d8027fc6410edeba8fe9874

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching (array, name) {
+    let newArray = array.filter( function (match) {
+            return name.toUpperCase() === match.toUpperCase();
+    });
+    return newArray;
+}
+
+function fuzzyMatch (array, char) {
+    let num = char.length;
+    let newArray = array.filter(function (name) {
+        if (name.slice(0,num) === char) {
+            return name;
+       }
+    })
+    return newArray;
+}
+
+function matchName (array, name) {
+    let newArray = array.filter(function (driver) {
+            return name === driver.name;
+    })
+    return newArray;
+}
+
+q = ['ab', 'bb', 'ab', 'bb', 'cb'];
+console.log(findMatching(q, 'bB'));
